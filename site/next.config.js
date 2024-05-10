612cc040e79b80662eb1f61fd151aea9c194c843
Chris Hunt <chris.hunt@kasmweb.com>
2024-05-10 08:54:46 +0100

    Update to next 14 and increase workspace compatibility

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export',
+  distDir: '../public',
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
